716fe289066388e5112ccc38ce6a78bc6eaf11c7
vguigue <vincent.guigue@gmail.com>
2024-12-11 13:52:54 +0100

    rnn

---
+++ newRNN/utils.py
@@ -0,0 +1,85 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+class RNN(nn.Module):
+    # [[STUDENT]] Implémenter comme décrit dans la question 1
+    def __init__(self, inputdim,latentdim):
+        super(RNN,self).__init__()
+        self.inputdim,self.latentdim= inputdim,latentdim
+        self.encoder = nn.Linear(inputdim,latentdim)
+        self.latent = nn.Linear(latentdim,latentdim)
+    def forward(self,x,h=None):
+        """ x: length x batch x dim """
+        hseq = []
+        if h is None:
+            h = self.hzero(x.shape[1]).to(x.device)
+        for i in range(x.shape[0]):
+            h = self.one_step(x[i],h)
+            hseq.append(h)
+        return torch.stack(hseq)
+    def one_step(self,x,h):
+        return  torch.tanh(self.encode(x)+self.latent(h))
+    def encode(self,x):
+        return self.encoder(x.view(-1,self.inputdim))
+    def hzero(self,batch_size):
+        return torch.zeros(batch_size,self.latentdim)
+
+    # [[/STUDENT]]
+    pass
+
+class SampleMetroDataset(Dataset):
+    def __init__(self, data,length=20,stations_max=None):
+        """
+            * data : tenseur des données au format  Nb_days x Nb_slots x Nb_Stations x {In,Out}
+            * length : longueur des séquences d'exemple
+            * stations_max : normalisation à appliquer 
+        """
+        self.data, self.length= data, length
+        ## Si pas de normalisation passée en entrée, calcul du max du flux entrant/sortant
+        self.stations_max = stations_max if stations_max is not None else torch.max(self.data.view(-1,self.data.size(2),self.data.size(3)),0)[0]
+        ## Normalisation des données
+        self.data = self.data / self.stations_max
+        self.nb_days, self.nb_timeslots, self.classes = self.data.size(0), self.data.size(1), self.data.size(2)
+
+    def __len__(self):
+        ## longueur en fonction de la longueur considérée des séquences 
+        return self.classes*self.nb_days*(self.nb_timeslots - self.length)
+
+    def __getitem__(self,i):
+        ## transformation de l'index 1d vers une indexation 3d
+        ## renvoie une séquence de longueur length et l'id de la station.
+        station = i // ((self.nb_timeslots-self.length) * self.nb_days)
+        i = i % ((self.nb_timeslots-self.length) * self.nb_days)
+        timeslot = i // self.nb_days
+        day = i % self.nb_days
+        return self.data[day,timeslot:(timeslot+self.length),station],station
+
+class ForecastMetroDataset(Dataset):
+    def __init__(self, data,length=20,stations_max=None):
+        """
+            * data : tenseur des données au format  Nb_days x Nb_slots x Nb_Stations x {In,Out}
+            * length : longueur des séquences d'exemple
+            * stations_max : normalisation à appliquer 
+        """
+        self.data, self.length= data,length
+        ## Si pas de normalisation passée en entrée, calcul du max du flux entrant/sortant
+        self.stations_max = stations_max if stations_max is not None else torch.max(self.data.view(-1,self.data.size(2),self.data.size(3)),0)[0]
+        ## Normalisation des données
+        self.data = self.data / self.stations_max
+        self.nb_days, self.nb_timeslots, self.classes = self.data.size(0), self.data.size(1), self.data.size(2)
+
+    def __len__(self):
+        ## longueur en fonction de la longueur considérée des séquences 
+        return self.nb_days*(self.nb_timeslots - self.length)
+
+    def __getitem__(self,i):
+        ## Transformation de l'indexation 1d vers indexation 2d
+        ## renvoie x[d,t:t+length-1,:,:], x[d,t+1:t+length,:,:]
+        timeslot = i // self.nb_days
+        day = i % self.nb_days
+        return self.data[day,timeslot:(timeslot+self.length-1)],self.data[day,(timeslot+1):(timeslot+self.length)]
+
